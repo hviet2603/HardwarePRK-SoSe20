ea3ff7f2061be5ce4ae9a21f05148d216218504f
viet <vincenthp2603@gmail.com>
2020-05-28 01:41:59 +0200

    28.05

--- Blatt03/Vivado_WORK/Tutorial/src/ArmMemInterface.vhd
+++ Blatt03/Vivado_WORK/Tutorial/src/ArmMemInterface.vhd
@@ -9,6 +9,8 @@ use ieee.std_logic_1164.all;
 
 library work;
 use work.ArmConfiguration.all;
+use work.ArmRAMB_4kx32.all;
+use work.armtypes.all;
 
 entity ArmMemInterface is
 	generic(
@@ -35,7 +37,41 @@ entity ArmMemInterface is
 end entity ArmMemInterface;
 
 architecture behave of ArmMemInterface is	
-
-begin	
+    signal OUTPUT_A: std_logic_vector(31 downto 0);
+    signal OUTPUT_B: std_logic_vector(31 downto 0);
+    signal WEB_SIGNAL: std_logic;
+begin
+    ID <= OUTPUT_A     when IDE = 1 else
+          x"XXXXXXXX"                   ;
+    
+    RAM_BLOCK: entity work.ArmRAMB_4kx32 
+    port map (
+    	RAM_CLK => RAM_CLK,	
+        ENA	=> IDE,	
+		ADDRA => IA(13 downto 2),	
+        WEB	=> WEB_SIGNAL,	
+        ENB	=> DDE,	
+        ADDRB => DA(13 downto 2), -- Byteaddress -> Wordaddress	
+        DIB	=>  DDIN,	
+        DOA	=> OUTPUT_A,	
+        DOB	=>	OUTPUT_B
+    );
+    
+    DDOUT <= OUTPUT_B when DDE = '1' and DnRW = '0' else
+             x"XXXXXXXX"                                ;    
+    
+    DABORT <= 'X' when DDE = '0'                                                 else
+              '1' when DDE = '1' and DMAS = DMAS_HWORD and DA(0) /= '0'          else
+              '1' when DDE = '1' and DMAS = DMAS_WORD and DA(1 downto 0) /= "00" else
+              '0';
+    
+    WEB_SIGNAL <= "1111" when DMAS = DMAS_WORD and DA(1 downto 0) = "00"  else -- Word: Address is divisible by 4
+                  "1100" when DMAS = DMAS_HWORD and DA(1 downto 0) = "00" else -- Halfword - Case 1: first half
+                  "0011" when DMAS = DMAS_HWORD and DA(1 downto 0) = "10" else -- Halfword - Case 2: second half
+                  "1000" when DMAS = DMAS_BYTE and DA(1 downto 0) = "00"  else -- Byte - Case 1: 1.Byte 
+                  "0100" when DMAS = DMAS_BYTE and DA(1 downto 0) = "01"  else -- Byte - Case 2: 2.Byte
+                  "0010" when DMAS = DMAS_BYTE and DA(1 downto 0) = "10"  else -- Byte - Case 3: 3.Byte
+                  "0001" when DMAS = DMAS_BYTE and DA(1 downto 0) = "11"  else -- Byte - Case 4: 4.Byte
+                  "0000"                                                      ;
 
 end architecture behave;
--- Blatt03/Vivado_WORK/Tutorial/src/ArmRAMB_4kx32.vhd
+++ Blatt03/Vivado_WORK/Tutorial/src/ArmRAMB_4kx32.vhd
@@ -9,6 +9,9 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+library work;
+use work.ArmRAMB_4kx8.all;
+
 entity ArmRAMB_4kx32 is
 	generic(
 --------------------------------------------------------------------------------
